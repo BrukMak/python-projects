e99fe939001c7110474dfb9de85e1337885fe00a
Bruk Mekonnen <brukmakeni@gmail.com>
2024-10-20 10:56:27 +0300

    Worked on automation and testig with selenium

---
+++ DSA/Exercise Files/06_03_begin/priority_queue.py
@@ -0,0 +1,31 @@
+"""
+Python Data Structures - A Game-Based Approach
+Priority Queue Class based on heapq.
+Robin Andrews - https://compucademy.net/
+"""
+
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self) -> None:
+        self.elements = []
+    def is_empty(self):
+        return not self.elements
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+    def __str__(self) -> str:
+        return str(self.elements)
+    
+if __name__ == "__main__":
+    pq = PriorityQueue()
+    pq.put("chill", 2)
+    pq.put("work", 1)
+    pq.put("sleep", 3)
+    print(pq)
+    print(pq.get())
+    print(pq.get())
+    print(pq.get())
+

03c9492d5bc3f01c9131efea656476b298c54dda
BrukMak <brukmakeni@gmail.com>
2024-12-11 16:05:54 +0300

    Covered path parameter, query parameter, request body and combination of the three.

--- FastAPI/One/main.py
+++ FastAPI/One/main.py
@@ -13,4 +13,17 @@ async def read_item(item_id: int):
 # Query Parameters
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
+    print(skip, limit)
     return fake_items_db[skip : skip + limit]
+
+# Type changing query parameters
+@app.get("/item/{item_id}")
+async def read_item_with_type_change(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
---
+++ FastAPI/One/request.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    result = {"item_id": item_id, **item.dict()}
+    return result
+
